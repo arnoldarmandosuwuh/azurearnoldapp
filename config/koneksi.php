185a84750d76970ed1a47474b2d6a0c9b54b60d4
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2019-04-08 16:58:39 +0700

    Azure Dicoding Submission 1

---
+++ config/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+//Koneksi kedatabase
+$hostmysql="arnoldazureapp.mysql.database.azure.com";
+$username_db="arnold@arnoldazureapp";
+$password_db="05121995As" ;
+$database="db_azure" ;
+$conn=mysqli_init();
+mysqli_real_connect($conn, $hostmysql, $username_db, $password_db, $database, 3306);
+if (mysqli_connect_errno($conn)) {
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
+?>
---
+++ pages/sidebar.php
@@ -0,0 +1,34 @@
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="?page=home"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-user fa-fw"></i> User<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="?page=user"><i class="fa fa-table fa-fw"></i> Data User</a>
+                                </li>
+                                <li>
+                                    <a href="?page=formuser"><i class="fa fa-edit fa-fw"></i> Input User</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
